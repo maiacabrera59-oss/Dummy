9dfd3b5c6dc566787578c5866b8ff28f172a19c6
maiacabrera59-oss <maiacabrera59@gmail.com>
2025-12-24 10:08:58 -0300

    prueba

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -125,7 +125,7 @@ export function Login() {
               Ingresando...
             </>
           ) : (
-            "Iniciar Sesión"
+            "Iniciar "
           )}
         </button>
       </form>
